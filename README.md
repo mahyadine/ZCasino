fd01f8841a6dd00d8ed2ede250a6f2301f4f8e50
mahyadine <yassimahyadine@gmail.com>
2019-11-14 15:35:16 +0100

    first commit and import the libraries

---
+++ README.md
@@ -0,0 +1,3 @@
+**Project Zcasino**
+
+This is a project on OppenClassrooms. He is develloped on python. The project takes the roulette rules.
---
+++ ZCasino.py
@@ -0,0 +1,9 @@
+import os
+import random 
+from math import ceil
+
+
+user_dollar : 100
+while user_dollar > 0:
+    mise = int(input("Veuillez entrer votre mise : "))
+    
